0497a9ef9abd8b3660bd2ae2222f5c565bcb39f3
Vibhor Sharma <165499128+vibhorSharma1@users.noreply.github.com>
2024-11-11 22:12:02 +0530

    Update carPannel.java

--- carPannel.java
+++ carPannel.java
@@ -1,30 +1,31 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class carPannel extends JPanel {
     ImageIcon gif;
     BufferedImage car;
-    BufferedImage car1;
-    BufferedImage car2;
-    BufferedImage car3;
+    List<Car> hudleCars;
     Timer timer;
     boolean isGameOver=false;
     int xAxis=255;
     int yAxis=400;
-    public int getRandom(){
-        int[] fcaxis={100,160,220,280,340,400};
-        int in= (int) Math.floor(Math.random()*6);
-        return fcaxis[in];
-    }
-    int fcyAxis=0,fcx1Axis= getRandom(),fcx2Axis= getRandom(),fcx3Axis= getRandom();
-    
+    int passCount=0;
+    Random random = new Random();
+    int time=random.nextInt(1501)+500;
     carPannel(){
         setSize(500, 500);
+        hudleCars=new ArrayList<>();
         showBg();
         loopPaint();
         keyboard();
@@ -34,9 +35,6 @@ public class carPannel extends JPanel {
         try{
             car=ImageIO.read(carPannel.class.getResource("upCAR.jpg"));
             gif=new ImageIcon(carPannel.class.getResource("road.gif"));
-            car1=ImageIO.read(carPannel.class.getResource("downCAR.jpg")) ;
-            car2=ImageIO.read(carPannel.class.getResource("downCAR.jpg"));
-            car3=ImageIO.read(carPannel.class.getResource("downCAR.jpg"));
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -47,30 +45,30 @@ public class carPannel extends JPanel {
         super.paintComponent(g);
         g.drawImage(gif.getImage(),20,0,500,500,null);
         g.drawImage(car,xAxis,yAxis,50,100,null);
-        g.drawImage(car1,fcx1Axis,fcyAxis,50,100,null);
-        g.drawImage(car2,fcx2Axis,fcyAxis,50,100,null);
-        g.drawImage(car3,fcx3Axis,fcyAxis,50,100,null);
-        
+        for(Car hurdelcar:hudleCars){
+            g.drawImage(hurdelcar.carImage, hurdelcar.carXaxis, hurdelcar.carYaxis, 50, 70, hurdelcar);   
+        }
+        g.setColor(Color.WHITE); 
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+         g.drawString("Passed Cars: " + passCount, 20, 20);
     }
     void loopPaint() {
         timer = new Timer(50, (a) -> {
-            if(isCollide(xAxis, yAxis, fcyAxis, fcx1Axis, fcx2Axis, fcx3Axis)){
+            if(isCollide(xAxis, yAxis, hudleCars)){
                 timer.stop();
                 isGameOver=true;
                 JOptionPane.showMessageDialog(this, "Game Over!");
             }
-            if(fcyAxis>400){
-                fcyAxis=0;
-                fcx1Axis= getRandom();
-                fcx2Axis= getRandom();
-                fcx3Axis= getRandom();
-            }else{
-                fcyAxis+=5;
-            }
+            moveCars();
             repaint();
         });
         timer.start();
-    }
+        Timer hurdleTimer = new Timer(time, (e) -> { 
+            hudleCars.add(new Car()); 
+        }); 
+        hurdleTimer.start(); 
+       }
+    
     public void keyboard() {
         System.out.println("keyboard called");
         
@@ -125,37 +123,36 @@ public class carPannel extends JPanel {
         });
     }
     
-    public boolean isCollide(int xAxis, int yAxis, int fcyAxis, int fcx1Axis, int fcx2Axis, int fcx3Axis) {
-        int carWidth = 50;  // Width of the cars
-        int carHeight = 100; // Height of the cars
-    
-        // Define the boundaries for the main car
-        int carX1 = xAxis;
-        int carX2 = xAxis + carWidth;
-        int carY1 = yAxis;
-        int carY2 = yAxis + carHeight;
-    
-        // Define boundaries for each of the three other cars
-        int[][] otherCars = {
-            {fcx1Axis, fcyAxis, fcx1Axis + carWidth, fcyAxis + carHeight},
-            {fcx2Axis, fcyAxis, fcx2Axis + carWidth, fcyAxis + carHeight},
-            {fcx3Axis, fcyAxis, fcx3Axis + carWidth, fcyAxis + carHeight}
-        };
-    
-        // Check collision with each of the other cars
-        for (int[] car : otherCars) {
-            int otherCarX1 = car[0];
-            int otherCarX2 = car[2];
-            int otherCarY1 = car[1];
-            int otherCarY2 = car[3];
-    
-            // Check if there's overlap in both x and y ranges
-            if (carX1 < otherCarX2 && carX2 > otherCarX1 && carY1 < otherCarY2 && carY2 > otherCarY1) {
-                return true;  // Collision detected
-            }
+    public boolean isCollide(int xAxis,int yAxis, List<Car> hurdleCars) { 
+        int carWidth = 50; 
+        int carHeight = 72; 
+         int carX1 = xAxis; 
+         int carX2 = xAxis + carWidth; 
+         int carY1 = yAxis; 
+         int carY2 = yAxis + carHeight; 
+         for (Car hurdleCar : hurdleCars) { 
+            int otherCarX1 = hurdleCar.carXaxis; 
+            int otherCarX2 = hurdleCar.carXaxis+ carWidth; 
+            int otherCarY1 = hurdleCar.carYaxis; 
+            int otherCarY2 = hurdleCar.carYaxis + carHeight; 
+            if (carX1 < otherCarX2 && carX2 > otherCarX1 && carY1 < otherCarY2 && carY2 > otherCarY1) { 
+                return true; 
+            } 
+        }
+         return false; 
         }
     
-        return false;  // No collision detected
-    }
+    public void moveCars() { 
+        int carSpeed = 5; 
+        List<Car> toRemove = new ArrayList<>(); 
+        for (Car hurdleCar : hudleCars) { 
+            hurdleCar.carYaxis += carSpeed; 
+            if (hurdleCar.carYaxis > 450) { 
+                toRemove.add(hurdleCar);
+                passCount++; 
+            } 
+        } 
+        hudleCars.removeAll(toRemove);
+     }
 }
 
